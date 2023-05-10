40e0a38f4c2eb52f4df3f0224ed612caa8f5ac89
sltsheryl <sltsheryl@gmail.com>
2023-05-10 16:00:55 +0800

    Add components

---
+++ meetup/src/App.js
@@ -0,0 +1,12 @@
+import Card from "./components/Card";
+  function App() {
+  return (
+    <div>
+      <h1>List</h1>
+      <Card text="Card 1" />
+      <Card text="Card 2" />
+    </div>
+  );
+}
+
+export default App;
